36c4b6a5ab8b7ebad55cb6b81eb85a6fcd392dc4
SWojcik800 <szymonwojcik800@gmail.com>
2024-12-17 21:22:04 +0100

    Fix: Example

--- SqlBuildingExtensions.Lib/SqlBuildingExtionsions.Examples/Program.cs
+++ SqlBuildingExtensions.Lib/SqlBuildingExtionsions.Examples/Program.cs
@@ -3,7 +3,7 @@
     "Column1",
     ("Column2", "AliasedColumn2"),
     "COUNT(*)",
-    ("(SELECT COUNT(*) FROM SomeOtherTable", "OtherTableExpression"),
+    ("(SELECT COUNT(*) FROM SomeOtherTable)", "OtherTableExpression"),
     SqlBuildingExtensions.Lib.SqlBuildingExtensions.Alias("Expr", "AliasedExpr")
     )
     } FROM TableName";
