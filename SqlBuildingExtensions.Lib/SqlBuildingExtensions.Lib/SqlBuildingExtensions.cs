8fa674cc9753b1632a2247ca92b480879087acf8
SWojcik800 <szymonwojcik800@gmail.com>
2024-12-17 21:08:34 +0100

    Initial commit

---
+++ SqlBuildingExtensions.Lib/SqlBuildingExtensions.Lib/SqlBuildingExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+
+namespace SqlBuildingExtensions.Lib
+{
+    public static class SqlBuildingExtensions
+    {
+        public static string SelectExpressions(params object[] cols)
+        {
+            StringBuilder sb = new StringBuilder(" ");
+            for (int i = 0; i < cols.Length; i++)
+            {
+                var col = cols[i];
+                if (col is ValueTuple<string, string>)
+                {
+                    var tuple = (ValueTuple<string, string>)col;
+                    var colName = tuple.Item1;
+                    var alias = tuple.Item2;
+                    sb.Append(colName);
+                    sb.Append(" AS ");
+                    sb.Append(alias);
+                }
+                else if (col is ValueTuple<string, string>)
+                {
+                    var tuple = (Tuple<string, string>)col;
+                    var colName = tuple.Item1;
+                    var alias = tuple.Item2;
+                    sb.Append(colName);
+                    sb.Append(" AS ");
+                    sb.Append(alias);
+                }
+                else if (col is string)
+                {
+                    var colName = col.ToString();
+
+                    sb.Append(colName);
+                }
+                else
+                {
+                    throw new ArgumentException($"Invalid parameter type: {col.GetType().Name}. Parameter index: {i}");
+                }
+
+                if (i < cols.Length - 1)
+                {
+                    sb.Append(", ");
+                }
+            }
+
+            return sb.ToString();
+
+
+        }
+    }
+
+}
---
+++ SqlBuildingExtensions.Lib/SqlBuildingExtionsions.Examples/Program.cs
@@ -0,0 +1,10 @@
+﻿var sql = $@"SELECT 
+{SqlBuildingExtensions.Lib.SqlBuildingExtensions.SelectExpressions(
+    "Column1",
+    ("Column2", "AliasedColumn2"),
+    "COUNT(*)",
+    ("(SELECT COUNT(*) FROM SomeOtherTable", "OtherTableExpression")
+    )
+    } FROM TableName";
+
+Console.WriteLine(sql);
